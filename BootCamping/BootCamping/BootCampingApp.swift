63ba0c81346571fcf76e82f67b450fd632fc102c
erun94510 <erun9414@icloud.com>
2022-12-16 17:08:43 +0900

    add project

---
+++ BootCamping/BootCamping/BootCampingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootCampingApp.swift
+//  BootCamping
+//
+//  Created by Donghoon Bae on 2022/12/16.
+//
+
+import SwiftUI
+
+@main
+struct BootCampingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
