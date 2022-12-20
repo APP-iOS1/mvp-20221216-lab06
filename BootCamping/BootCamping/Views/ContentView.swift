d7549feec2f197ad4bfabd7680682412c8eba86c
Primrose1101 <114223237+Primrose1101@users.noreply.github.com>
2022-12-20 18:07:32 +0900

    221220 홈뷰, 마이페이지

--- BootCamping/BootCamping/Views/ContentView.swift
+++ BootCamping/BootCamping/Views/ContentView.swift
@@ -7,6 +7,11 @@
 
 import SwiftUI
 
+//enum tapInfo : String, CaseIterable {
+//    case top = "오늘의 캠핑"
+//    case follow = "팔로우중인 캠퍼"
+//}
+
 struct ContentView: View {
     var body: some View {
         VStack {
@@ -19,8 +24,8 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+//struct ContentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ContentView()
+//    }
+//}
