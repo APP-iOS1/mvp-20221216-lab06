519aa0c81ee70905f475296df00a45d2a1568a32
thekoon0456 <thekoon0456@gmail.com>
2022-12-23 16:18:28 +0900

    add communutyStore

---
+++ BootCamping/BootCamping/Models/CommunityPost.swift
@@ -0,0 +1,32 @@
+//
+//  Community.swift
+//  BootCamping
+//
+//  Created by Donghoon Bae on 2022/12/23.
+//
+
+import Foundation
+import Firebase
+
+struct CommunityPost {
+    var id: String
+    var userID: String
+    var title: String
+    var content: String
+    var createdDate: Timestamp
+    var photos: [String]
+    var category: [String]
+}
+
+
+struct CommunityComments {
+    var id: String
+    var userID: String
+    var communityCommentContent: String
+    var communityCommentCreatedDate: Timestamp
+}
+
+struct CommunityLikes {
+    var id: String
+    var userID: String
+}
